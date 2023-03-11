8c5e4a2007ce5a95d1d0caa897dd257f96898e4c
wangyibing <649756291@qq.com>
2023-03-11 18:58:32 +0800

    新增

--- clent.cpp
+++ clent.cpp
@@ -14,8 +14,42 @@ int main() {
 		perror("socket");
 		exit(-1);
 	}
+	struct  sockaddr_in clentaddr;
+	memset(&clentaddr, 0, sizeof(clentaddr));
+	clentaddr.sin_family = AF_INET;
+	inet_pton(AF_INET, "127.0.0.1", &clentaddr.sin_addr.s_addr);
+	clentaddr.sin_port = htons(8888);
+	int ret = connect(fd, (struct sockaddr *)&clentaddr, sizeof(clentaddr));
+	if (ret == -1) {
+		cout << "客户端connect连接失败 \n";
+		perror("connect");
+		exit(-1);
+	}
+	// 3 通信
 
+	 char recvBuf[1024] = {};
+	 char client_data[1024];
+	 while(1) {
+	 	memset(client_data,0,sizeof(client_data));
+	 	printf("请输入消息\n");
+	 	// scanf("%s", client_data);
+	 	cin >> client_data;
+	 	write(fd, client_data, strlen(client_data));
+	 	sleep(1);
+	 	int ret = read(fd, recvBuf,sizeof(recvBuf));
+	 	if (ret == -1) {
+	 		perror("read");
+	 		exit(-1);
+	 	} else if (ret > 0) {
+	 		printf(" 服务器信息 %s\n", recvBuf);
+	 		continue;
+	 	} else if (recvBuf == "exit") {
+	 		printf(" 断开连接\n");
+	 		break;
+	 	}
 
+	 }
+	 close(fd);
 	return 0;
 }
 // 静态变量作用
